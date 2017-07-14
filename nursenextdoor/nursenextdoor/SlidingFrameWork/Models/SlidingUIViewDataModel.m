1a81b46499976eb8295b02b189345164b22e87c3
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-07-13 18:16:24 -0700

    adding the animation to the code, it goes up and down. Now I need to add a function to return back if no new UIView came up, and

--- nursenextdoor/nursenextdoor/SlidingFrameWork/Models/SlidingUIViewDataModel.m
+++ nursenextdoor/nursenextdoor/SlidingFrameWork/Models/SlidingUIViewDataModel.m
@@ -62,8 +62,70 @@
     NSString *newDateString = [outputFormatter stringFromDate:now];
     NSLog(@"newDateString %@", newDateString);
 
+
+//
+
+    
+    
+            
+
+    
+            // Transition sequence
+            [UIView beginAnimations:@"ShowHideView" context:nil];
+            [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+            [UIView setAnimationDuration:0.250];
+            [UIView setAnimationDelegate:self];
+            [UIView setAnimationDidStopSelector:@selector(showHideDidStop:finished:context:)];
+    
+            // Make the animatable changes.
+            // _leftUiView.alpha = 1.0;
+            // _rightUiView.alpha = 0.0;
+    
+    
+            // Transition Direction
+            _templateOfMasterUIView.frame = CGRectMake(0, -100,[UIApplication sharedApplication].delegate.window.frame.size.height, [UIApplication sharedApplication].delegate.window.frame.size.width);
+    
+            // Commit the changes and perform the animation.
+            [UIView commitAnimations];
+
+    //
+
+
+
+
+
+
     return nil;
 
 }
+
+
+
+- (void)showHideDidStop:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context {
+        /*
+         Author: Kian D.RAd
+         Date:   Jun 30th 2017
+         README: This is a delegate like method that informs me, when the transition or
+         animation have been completed. So based on this accurately take the next step */
+    
+        // Set book value to false, so the progam knows that the transition was finished.
+        // This is for internal use, if you wanna use publicly use, the Objective C method.
+
+    
+        // Test Animation
+        [UIView beginAnimations:@"ShowHideView2" context:nil];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+        [UIView setAnimationDuration:0.25];
+        [UIView setAnimationDelay:0.5];
+    
+        //    _rightUiView.alpha = 1.0;
+        _templateOfMasterUIView.frame = [UIApplication sharedApplication].delegate.window.frame;
+    
+        [UIView commitAnimations];
+    
+    
+    }
+
+
 
 @end
